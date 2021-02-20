4dfd6976ecb0ae46e41c14ad973f3a29b48754de
Jimmy Tan <jtan5372@gmail.com>
2021-02-19 22:50:45 -0500

    Revert "Revert "Added animations""
    This reverts commit d757f62d4349c42f26c5b3f9918ec429758ebca1.

--- DuskAndDawn/Assets/Scripts/ThreeDMovement.cs
+++ DuskAndDawn/Assets/Scripts/ThreeDMovement.cs
@@ -56,7 +56,11 @@ public class ThreeDMovement : MonoBehaviour
 
     private void RotateTowardMovementVector(Vector3 movementVector)
     {
-        if(movementVector.magnitude == 0) { return; }
+        if(movementVector.magnitude == 0) {
+            anim.SetBool("isRunning", false);
+            return; 
+        }
+        anim.SetBool("isRunning", true);
         print(movementVector);
         var rotation = Quaternion.LookRotation(new Vector3(movementVector.z, 0, -movementVector.x));
         transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotateSpeed);
