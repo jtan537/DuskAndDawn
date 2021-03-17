151a75e96c56f9e7f7cd49662c9d4e1d7d199f01
JZ-Z-Z <haosun.zhou@mail.utoronto.ca>
2021-03-17 18:22:55 -0400

    TELEPORT

---
+++ DuskAndDawn/Assets/tower/script/Teleport.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    // private float distance;
+    // public float a;
+    public GameObject player;
+    // public Transform target;
+    private bool canTeleport = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canTeleport){
+            if (Input.GetKeyDown(KeyCode.F)){
+                player.transform.position = gameObject.transform.GetChild(0).gameObject.transform.position;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // if (other.gameObject.tag == "player"){
+            print("can teleport");
+            canTeleport = true;
+        // }
+    }
+
+    private void OnTriggerExit(Collider other) 
+    {
+        // if (other.gameObject.tag == "player"){
+            print("can not teleport");
+            canTeleport = false;
+        // }
+    }
+
+
+}
