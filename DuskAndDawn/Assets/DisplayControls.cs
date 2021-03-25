577a1c2ca190a54e27de60c0d49f671ac1c0d194
jtan537 <jtan5372@gmail.com>
2021-03-25 01:07:25 -0400

    added controls ui in ice puzzle

--- DuskAndDawn/Assets/DisplayControls.cs
+++ DuskAndDawn/Assets/DisplayControls.cs
@@ -7,9 +7,10 @@ public class DisplayControls : MonoBehaviour
 {
     // Start is called before the first frame update
     bool show = true;
+    public bool startShow = false;
     void Start()
     {
-        gameObject.GetComponent<Image>().enabled = true;
+        gameObject.GetComponent<Image>().enabled = startShow;
         gameObject.GetComponent<CanvasRenderer>().SetAlpha(1.0f);
         
     }
