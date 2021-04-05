2f6e95de8e026c34f33cd0bbcb8fd1bdbe98c6d1
jtan537 <jtan5372@gmail.com>
2021-04-05 19:36:24 -0400

    set up trail support

---
+++ DuskAndDawn/Assets/RoslynDirectoryCreator.cs
@@ -0,0 +1,29 @@
+using System.IO;
+using System.Text.RegularExpressions;
+
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+[InitializeOnLoad]
+internal static class RoslynDirectoryCreator
+{
+    static RoslynDirectoryCreator() => Application.logMessageReceived += OnLogMessageReceived;
+
+    private static void OnLogMessageReceived(string message, string _, LogType logType)
+    {
+        if (logType != LogType.Exception)
+            return;
+
+        const string pattern =
+                    @"^DirectoryNotFoundException: Could not find " +
+                    @"a part of the path ('|"")Temp(\\|/)RoslynAnalysisRunner";
+
+        if (Regex.IsMatch(message, pattern))
+        {
+            Directory.CreateDirectory("Temp/RoslynAnalysisRunner");
+        }
+    }
+}
+#endif
